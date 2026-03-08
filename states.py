d564dc67c2fa6e2526f4e4e04515c1252826554a
bulbulcentrmsk-crypto <bulbulcentrmsk@gmail.com>
2026-03-09 02:43:30 +0300

    Update states.py

--- states.py
+++ states.py
@@ -4,12 +4,12 @@ from aiogram.dispatcher.filters.state import State, StatesGroup
 class AddAgent(StatesGroup):
     waiting_for_full_name = State()
     waiting_for_phone = State()
-    waiting_for_email = State()
+    waiting_for_place_of_work = State()  # место работы (был email)
 
 class AddRequest(StatesGroup):
     waiting_for_full_name = State()
+    waiting_for_birth_date = State()     # дата рождения
     waiting_for_phone = State()
-    waiting_for_email = State()
     waiting_for_center = State()
     waiting_for_message = State()
 
